1d4ecabeb6260cf63a620ee693b8b5f01c3a4e8d
dariush dehghani <r.dehghani.90@gmail.com>
2024-01-05 11:10:00 +0330

    write managecourselistview

--- e_learning/courses/views.py
+++ e_learning/courses/views.py
@@ -1,3 +1,14 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from .models import Course
+from django.views.generic.list import ListView
 
-# Create your views here.
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
