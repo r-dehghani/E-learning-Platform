e17ffcf17b47c47dfd816ef8f01df9d921857dd0
dariush dehghani <r.dehghani.90@gmail.com>
2024-01-26 10:13:23 +0330

    password reset feature is completed

--- e_learning/e_learning/settings.py
+++ e_learning/e_learning/settings.py
@@ -135,3 +135,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+# ======================= Sending Email settings ===========================
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
