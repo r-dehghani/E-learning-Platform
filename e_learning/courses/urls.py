b3ba404dd834d85da82526da6efb506ee3a40d96
dariush dehghani <r.dehghani.90@gmail.com>
2024-03-15 23:17:47 +0330

    urls of create and update content

--- e_learning/courses/urls.py
+++ e_learning/courses/urls.py
@@ -16,4 +16,9 @@ urlpatterns = [
          name='course_delete'),
      path('<pk>/module/', views.CourseModuleUpdateView.as_view(),
           name='course_module_update'),
+     path('module/<int:module_id>/content/<module_name>/create/', 
+          views.ContentCreateUpdateView.as_view(),
+          name='module_content_create'),
+     path('module/<int:module_id>/content/<module_name>/id/',
+          views.ContentCreateUpdateView.as_view()),
          ]
