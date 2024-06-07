c0e382ab885dafbc8bc845218025e90e66749a52
dariush dehghani <r.dehghani.90@gmail.com>
2024-06-07 22:22:57 +0330

    add pillow t orequirements.py

--- e_learning/e_learning/settings.py
+++ e_learning/e_learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4m^u*#li+(qsv%pm+byta!)fmqqc%-$14n9)u@40f(e@mv%7^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
