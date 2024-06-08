258e14b4762b4d57e7b2030a9b74f417cbfe6e0f
dariush dehghani <r.dehghani.90@gmail.com>
2024-06-08 21:30:26 +0330

    add super user

--- e_learning/build_files.sh
+++ e_learning/build_files.sh
@@ -1,3 +1,6 @@
 python3 -m ensurepip --upgrade
 pip3 install -r requirements.txt
+python3 manage.py createsuperuser
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3.12 manage.py collectstatic
